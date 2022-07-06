57fd0dfca6303209bc63e61fd46e0f1c2e3c318e
CoracaoDoMundo <lazakovich@gmail.com>
2022-07-06 20:05:20 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    let nAbs = Math.abs(n);
+    let numberString = nAbs.toString();
+
+    let i = 0;
+    let result = '';
+    
+    while (i < numberString.length) {
+      result = `${numberString[i]}${result}`;
+      i = i + 1;
+    }
+    let resultNumber = Number(result);
+    return resultNumber;
 }
